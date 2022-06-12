b7399eb5d6add48d83013c83ec16a0f1955bcaba
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2022-06-12 19:58:22 +0200

    Publish command added

---
+++ Source/CopyRouteMod/CLIUtils/CLIUtilsProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CLIUtils
+{
+    class CLIUtilsProgram
+    {
+        static int Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Expected more than 0 arguments");
+                return -2;
+            }
+
+            if (args[0].ToLower() == "publish")
+            {
+                Publisher.Publish();
+                return 0;
+            }
+            
+            Console.WriteLine($"Unknown argument {args[0]}");
+            return -3;
+        }
+    }
+}
---
+++ Source/CopyRouteMod/CLIUtils/Publisher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace CLIUtils
+{
+    public class Publisher
+    {
+        public static void Publish()
+        {
+            new Publisher().Publish_Internal();
+        }
+
+        private void Publish_Internal()
+        {
+            string baseDirectory = Path.Combine(Environment.CurrentDirectory, "../../../../");
+            CheckBaseDirectory(baseDirectory);
+            
+            string outputDirectory = Path.Combine(baseDirectory, "Output");
+            string outputTemplateDirectory = Path.Combine(baseDirectory, "Output_template");
+
+            Console.WriteLine("Cleaning output directory...");
+            Directory.Delete(outputDirectory, true);
+            Directory.CreateDirectory(outputDirectory);
+
+            Console.WriteLine("Copying Output Template to Output...");
+            CopyAllFiles(outputTemplateDirectory, outputDirectory);
+        }
+
+        private static void CopyAllFiles(string sourceDirPath, string targetDirPath)
+        {
+            DirectoryInfo sourceDir = new DirectoryInfo(sourceDirPath);
+
+            if (sourceDir.Exists == false)
+            {
+                throw new Exception("Failed to copy files: source directory does not exist");
+            }
+            
+            CopyAllFiles(sourceDir, new DirectoryInfo(targetDirPath));
+        }
+        
+        private static void CopyAllFiles(DirectoryInfo source, DirectoryInfo target) 
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                CopyAllFiles(dir, target.CreateSubdirectory(dir.Name));
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+            }
+        }
+
+        private static void CheckBaseDirectory(string baseDirectoryPath)
+        {
+            string baseDirectory = Path.Combine(baseDirectoryPath, "Source");
+            if (Directory.Exists(baseDirectory) == false)
+            {
+                // Sanity check - break if program ended up in wrong directory
+                throw new Exception($"Unexpected output directory path: '{baseDirectoryPath}' Full: {Path.GetFullPath(baseDirectoryPath)}");
+            }
+        }
+    }
+}
