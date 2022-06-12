a5389ed53925c3e0ebd07083e8e1485059dfc7e0
mateusz.krzaczek <mateusz.krzaczek@gmail.com>
2022-06-12 21:51:16 +0200

    STModLoaderInjection WIP

--- Source/CopyRouteMod/STModLoaderInjection/STModLoaderInjectionProgram.cs
+++ Source/CopyRouteMod/STModLoaderInjection/STModLoaderInjectionProgram.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using CLIUtils;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -12,12 +11,21 @@ namespace STModLoaderInjection
     {
         static void Main(string[] args)
         {
+            string stAssemblyPath = CreateModdedAssembly(out string moddedAssemblyPath);
+
+            ReplaceOriginalAssembly(stAssemblyPath, moddedAssemblyPath);
+            
+            Console.WriteLine("Done. Sweet Transit dll has been modded with MatiModLoader.");
+        }
+
+        private static string CreateModdedAssembly(out string moddedAssemblyPath)
+        {
             string stAssemblyPath = Path.Combine(GameDirectoryManager.GetGameDirectoryPath(), "Sweet Transit.dll");
 
             ReaderParameters readerParameters = new ReaderParameters(ReadingMode.Immediate);
             readerParameters.AssemblyResolver = new CustomResolver(GameDirectoryManager.GetGameDirectoryPath());
-            
-            ModuleDefinition module = ModuleDefinition.ReadModule(stAssemblyPath, readerParameters);
+
+            using ModuleDefinition module = ModuleDefinition.ReadModule(stAssemblyPath, readerParameters);
 
             TypeDefinition loadingType = module.GetType("Sweet_Transit.GameData", "Loading");
             MethodDefinition loadMethod = loadingType.Methods.Single(x => x.Name == "Load");
@@ -28,13 +36,43 @@ namespace STModLoaderInjection
             //var callModLoaderInstruction = loadMethodProcessor.Create(OpCodes.Call, loadModMethod);
 
             var newInstruction = loadMethodProcessor.Create(OpCodes.Nop);
-                
+
             var firstInstruction = loadMethod.Body.Instructions[0];
             loadMethodProcessor.InsertBefore(firstInstruction, newInstruction);
+
+            moddedAssemblyPath = Path.Combine(GameDirectoryManager.GetGameDirectoryPath(), "Sweet Transit.dll.modded");
+            module.Write(moddedAssemblyPath);
+            return stAssemblyPath;
+        }
+
+        private static void ReplaceOriginalAssembly(string originalAssemblyPath, string moddedAssemblyPath)
+        {
+            if (File.Exists(originalAssemblyPath) == false)
+            {
+                throw new Exception("Original assembly not found.");
+            }
             
-            module.Write("Sweet Transit.dll Modded");
-            
-            Console.WriteLine("Done. Sweet Transit dll has been modded with MatiModLoader.");
+            if(File.Exists(moddedAssemblyPath) == false)
+            {
+                throw new Exception("Modded assembly not found.");
+            }
+
+            TryToCreateAssemblyBackup(originalAssemblyPath);
+
+            File.Copy(moddedAssemblyPath, originalAssemblyPath, true);
+            File.Delete(moddedAssemblyPath);
+        }
+
+        private static void TryToCreateAssemblyBackup(string originalAssemblyPath)
+        {
+            string backupAssemblyPath = $"{originalAssemblyPath}.orig";
+
+            if (File.Exists(backupAssemblyPath))
+            {
+                return;
+            }
+
+            File.Copy(originalAssemblyPath, backupAssemblyPath);
         }
     }
 }
